e875d20aa841c48ba0b967ed09c6f76e8537f622
hyunas1996 <hyunas1996@gmail.com>
2023-01-11 17:06:13 +0900

    Start OpenGL Tutorials

---
+++ OpenGL_Tutorial_Bins_XCode/CMakeFiles/3.25.1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/hyunahome/Desktop/OpenGL_Tutorial/OpenGL_Tutorial_Bins_XCode/CMakeFiles/3.25.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ OpenGL_Tutorial_Bins_XCode/launch-tutorial06_keyboard_and_mouse-Debug.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+bindir=$(pwd)
+cd /Users/hyunahome/Desktop/OpenGL_Tutorial/ogl-master/tutorial06_keyboard_and_mouse/
+export 
+
+if test "x$1" = "x--debugger"; then
+	shift
+	if test "x" = "xYES"; then
+		echo "r  " > $bindir/gdbscript
+		echo "bt" >> $bindir/gdbscript
+		GDB_COMMAND-NOTFOUND -batch -command=$bindir/gdbscript  /Users/hyunahome/Desktop/OpenGL_Tutorial/OpenGL_Tutorial_Bins_XCode/Debug/tutorial06_keyboard_and_mouse 
+	else
+		"/Users/hyunahome/Desktop/OpenGL_Tutorial/OpenGL_Tutorial_Bins_XCode/Debug/tutorial06_keyboard_and_mouse"  
+	fi
+else
+	"/Users/hyunahome/Desktop/OpenGL_Tutorial/OpenGL_Tutorial_Bins_XCode/Debug/tutorial06_keyboard_and_mouse"  
+fi
